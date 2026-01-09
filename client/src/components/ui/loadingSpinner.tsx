cf311fa2d340a3deec6464e55ff4fba4c2805c3f
ffacun2 <facu-1999-44@hotmail.com>
2026-01-09 12:04:26 -0300

    loading spinner component

--- client/src/components/ui/loadingSpinner.tsx
+++ client/src/components/ui/loadingSpinner.tsx
@@ -1,9 +1,12 @@
 
-export default function LoadingSpinner() {
+export default function LoadingSpinner({ message }: { message: string }) {
 
-    return (
-        <div className="text-center py-12">
+   return (
+      <div className="flex items-center justify-center h-full">
+         <div className="flex flex-col items-center gap-2">
             <div className="inline-block w-8 h-8 border-4 border-gray-500 border-t-transparent rounded-full animate-spin"></div>
-        </div>
-    )
+            <p className="text-slate-500">{message}</p>
+         </div>
+      </div>
+   )
 }
